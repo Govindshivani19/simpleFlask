41bd91dfd5ccb1ff5c457c1c4b44e2a6830bc5be
shivani <shivani.govind@lntinfotech.com>
2021-08-27 22:04:57 +0530

    save

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import csv
+import glob
 import os
+import xlrd
 from flask import Flask, render_template, request
 import pandas as pd
+from xlrd.timemachine import xrange
+import numpy as np
+import missingno as msno
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
 
 arr_dataset = []
 
@@ -23,15 +39,66 @@ def data():
         elif extension_data[1] == ".xlsx":
             data = pd.read_excel(file.filename)
         elif extension_data[1] == ".xls":
-            data = pd.read_excel(file.filename)
+            wb = xlrd.open_workbook(file.filename)
+            sh = wb.sheet_by_name('Sheet1')
+            your_csv_file = open('your_csv_file.csv', 'wb')
+            wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+            for rownum in xrange(sh.nrows):
+                wr.writerow(sh.row_values(rownum))
+
+            your_csv_file.close()
+            # newName = extension_data[0] + ".csv"
+            # fileDir, fileName = os.path.split(file.filename)
+            # outCSV = os.path.join(fileDir, newName)
+            # for file_name in glob.iglob(file.filename):
+            #     file.save(outCSV)
+            #     os.remove(file_name)
+            data = pd.read_csv(file.filename)
         headings = data.columns.values
         for index, rows in data.iterrows():
             arr_dataset.append(rows.tolist())
 
         data_set = arr_dataset
-        os.remove(file.filename)
-        return render_template('index.html', headings=headings, data=data_set)
+        #os.remove(file.filename)
+        return render_template('excel_data.html', headings=headings, data=data_set)
 
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'GET':
+        dataset = pd.read_csv("C:\\Users\\Lenovo\\Downloads\\a.csv")
+        le = LabelEncoder()
+        dataset.Product = le.fit_transform(dataset.Product)
+        dataset.Component = le.fit_transform(dataset.Component)
+        dataset.Status = le.fit_transform(dataset.Status)
+        dataset.Assignee = le.fit_transform(dataset.Assignee)
+        dataset.Release = le.fit_transform(dataset.Release)
+        dataset.isnull().sum()
+        columns_of_sheet = dataset[
+            ['Product', 'Component', 'Status', 'Release', 'Severity']]
+        X = np.asarray(columns_of_sheet)
+        Y = np.asarray(dataset['Assignee'])
+        X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y,
+                                                            test_size=0.25,
+                                                            random_state=0)
+        sc_X = StandardScaler()
+        X_Train = sc_X.fit_transform(X_Train)
+        X_Test = sc_X.transform(X_Test)
+
+        classifier = SVC(kernel='linear', random_state=5, gamma='auto', C=4)
+        a = classifier.fit(X_Train, Y_Train)
+        print(a)
+
+        Y_Pred = classifier.predict(X_Test)
+        score = classifier.score(X_Train, Y_Train)
+        print(score * 100)
+        print(Y_Pred)
+
+        cm = confusion_matrix(Y_Test, Y_Pred)
+        print(cm)
+
+        return render_template('predict.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.1.1', port=5000)
